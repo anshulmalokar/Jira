4bc71cf447a9cc039315ea6d059dc592a4b0d61a
anshulmalokar <anshulmalokar@gmail.com>
2025-03-15 22:45:07 +0530

    handle image upload

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
   }
   return (
     <>
-      <div className="bg-neutral-500 p-4 h-full">
+      <div className="border p-4 h-full">
          <CreateWorkspaceForm/>
       </div>
     </>
--- src/features/workspaces/components/CreateWorkspaceForm.tsx
+++ src/features/workspaces/components/CreateWorkspaceForm.tsx
@@ -17,22 +17,46 @@ import { DottedSeparator } from '@/components/dotted-seperator';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { useCreateWorkspace } from '../api/use-createworkspace';
+import { useRef } from 'react';
+import { Avatar } from '@/components/ui/avatar';
+import Image from 'next/image';
+import { AvatarFallback } from '@radix-ui/react-avatar';
+import { ImageIcon } from 'lucide-react';
 
 type Props = {
     onCancel?: () => void,
 }
 
 export default function CreateWorkspaceForm({onCancel}: Props) {
+  const inputRef = useRef<HTMLInputElement>(null);
   const {mutate, isPending} = useCreateWorkspace();
   const form = useForm<z.infer<typeof workspaceSchema>>({
     resolver: zodResolver(workspaceSchema),
     defaultValues:{
         name: '',
+        image: ''
     }
   });
 
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if(file){
+        form.setValue("image", file);
+    }
+  };
+
   const onSubmit = (values: z.infer<typeof workspaceSchema>) => {
-    mutate({json: values});
+    console.log("Hello");
+    const finalValue = {
+        name: values.name,
+        image: values.image instanceof File ? values.image : ""
+    };
+    mutate({form: finalValue},{
+        onSuccess: () => {
+            form.reset();
+            // ToDo: ReDirect to new workspace
+        }
+    });
   }
 
   return (
@@ -65,6 +89,61 @@ export default function CreateWorkspaceForm({onCancel}: Props) {
                                 </FormItem>
                             )}>
                             </FormField>
+                            <FormField 
+                            control={form.control}
+                            name='image'
+                            render={({field}) => (
+                                <div className='flex flex-col gap-y-2'>
+                                    <div className='flex items-center gap-x-5'>
+                                        {
+                                            field.value ? (
+                                                <div className='size-[72px] rounded-md relative overflow-hidden'>
+                                                    <Image 
+                                                    src={
+                                                        field.value instanceof File ? URL.createObjectURL(field.value) 
+                                                        : field.value
+                                                    }
+                                                    alt='logo'
+                                                    fill
+                                                    className='object-cover'/>
+                                                </div>
+                                            ) : <>
+                                                <Avatar className='size-[72px]'>
+                                                    <AvatarFallback>
+                                                        <ImageIcon className='size-[36px] text-neutral-400'/>
+                                                    </AvatarFallback>
+                                                </Avatar>
+                                            </>
+                                        }
+                                        <div className='flex flex-col'>
+                                            <p className='text-sm'>WorkSpace Icon</p>
+                                            <p className='text-sm text-muted-foreground'>
+                                                JPG, PNG, SVG or JPEF, max 1mb
+                                            </p>
+                                            <input
+                                            className='hidden'
+                                            type='file'
+                                            accept='.jpg, .png, .jpeg, .svg'
+                                            ref={inputRef}
+                                            disabled={isPending}
+                                            onChange={handleImageChange}
+                                            />
+                                            <Button
+                                            disabled={isPending}
+                                            type='button'
+                                            variant={'secondary'}
+                                            size="sm"
+                                            className='w-full mt-2'
+                                            onClick={() => {
+                                                inputRef.current?.click()
+                                            }}>
+                                                Upload Image
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </div>
+                            )}>
+                            </FormField>
                             <div className='flex items-center justify-between'>
                                 <Button 
                                 disabled={isPending}
@@ -74,6 +153,7 @@ export default function CreateWorkspaceForm({onCancel}: Props) {
                                     Cancel
                                 </Button>
                                 <Button 
+                                type='submit'
                                 disabled={isPending}
                                 size={'lg'}
                                 onClick={onCancel}>
